3c40114c1c9985429b9960b884be9af78926b02a
vagadiya2005 <princevagadiya2005@gmail.com>
2024-06-29 11:53:42 +0530

    basic route setup and req.query

--- index.js
+++ index.js
@@ -1,55 +1,16 @@
-const { dir } = require('console');
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
 
-// craete file path with filepath and create newfile.txt
-const filepath = path.join(__dirname,'newFile.txt');
-const experimentFile = path.join(__dirname,'expFile.txt');
+const app = express();
 
-// create new file using writeFileSync
-fs.writeFileSync(filepath,'i am a new file');
+// basic rout setup
+app.get('/hello',(req,res)=>{
 
-// read file content utf8 must add 
-fs.readFile(filepath,'utf8',(err,item)=>{
-
-    // if error not occured then display item 
-    if(!err)
-console.log(item);
-
-    else
-    console.log('getting error');
+        // query send by client in request
+    console.log('data sending by client ---->',req.query);
+    
+    res.send('hello everyone again');
 
 })
 
-// append file content.
-fs.appendFileSync(filepath,' this content is appended');
-
-// direct create file and append text.
-fs.appendFileSync(experimentFile,'this is experimental file');
-
-// create new folder  path 
-const dirPath = path.join(__dirname,'newFolder');
-
-function CreateNewFolder(dirPath){
-
-
-    try{
-        // careate new dir 
-        fs.mkdirSync(dirPath, {recursive: true});
-        console.log('folder created');
-
-        // if dir not created then throw error
-    }catch(err){
-        console.log(`error generated : ${err.message}`);
-        
-    }
-
-    // function name and dir name
-}  CreateNewFolder('./newFolder');
-
- 
-
-
-
-
-
+// app.get('/hello',(req,res)=>{  listen request at port number 5500
+app.listen(5500);
