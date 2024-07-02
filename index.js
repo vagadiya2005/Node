bf50f095795c57d4c6c0f5aeaa85c4c241a227f7
vagadiya2005 <princevagadiya2005@gmail.com>
2024-07-02 21:55:43 +0530

    res.sendFile and use settimeout in request

--- index.js
+++ index.js
@@ -3,13 +3,35 @@ const path = require('path');
 
 const app = express();
 
-// select public folder
-const abouPath = path.join(__dirname,'public');
- 
-// send respons in form of html page
-app.use(express.static(abouPath)); // here abouPath is a folder path.
-// app.use(express.static('public'));  Here express.static load static pages.
+const htmlPath = path.join(__dirname,'public');
 
 
-// send html page which present in public folder localhost:port/ex.html
-app.listen(5500);   
+app.get('',(req,res)=>{
+
+res.sendFile(`${htmlPath}/index.html`);
+
+})
+
+app.get('/about',(req,res)=>{
+
+res.sendFile(`${htmlPath}/about.html`);
+
+})
+
+app.get('/help',(req,res)=>{
+
+    setTimeout(() => {
+       res.sendFile(`${htmlPath}/help.html`);
+    }, 4000);
+
+});
+
+
+
+app.get('*',(req,res)=>{
+
+res.sendFile(`${htmlPath}/page404.html`);
+
+})
+
+app.listen(5500);
