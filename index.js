f5d38c39ed285af389d7831ddfa0ff72083aa594
vagadiya2005 <princevagadiya2005@gmail.com>
2024-07-13 13:01:43 +0530

    crud opration with mongoose insert,update,delete,find

--- index.js
+++ index.js
@@ -1,28 +1,93 @@
-const mongoose = require("mongoose");
+const { MongoGCPError } = require('mongodb');
+const mongoose = require('mongoose');
 
-const main = async () => {
-  await mongoose.connect(
-    "mongodb+srv://23ce140:cxFGrQJU8OeTCXVG@cluster0.paezpii.mongodb.net/example"
-  );
-  console.log("connected sucessfully");
 
-  const ProductSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-  });
+const MongooseConnect = async()=>{
+
+  const url = 'mongodb+srv://23ce140:cxFGrQJU8OeTCXVG@cluster0.paezpii.mongodb.net/example';
+
+  await mongoose.connect(url);
+  console.log('Connected.');
   
-  // Here "products" is a collection of DB. and ProductSchema is a Schema of our data collection.
-  const ProductModel = mongoose.model("products", ProductSchema);
-
-    // create data with model named "ProductModel".
-  const data = new ProductModel({ name: "oppo", price: 5000 , sex:"mela"});
-
-  // save data in DB.
-  let result = await data.save();
+}
 
 
-  console.log(result);
 
-};
+const ProductSchema = mongoose.Schema({
 
-main();
+name: String,
+prize: Number,
+brand: String,
+category: String
+
+})
+
+
+
+const insetData = async()=>{
+
+
+const Product = mongoose.model('products',ProductSchema); // create model.
+
+let data =  new Product({name:'mi note 11 pro max',prize:23000 , brand:'MI' });
+
+let result = await data.save();
+
+console.log(result);
+
+}
+
+const updateData = async()=>{
+
+  MongooseConnect();
+
+  const Product = mongoose.model('products',ProductSchema); // create model.
+  
+  let data = await Product.updateOne( // as well as .updateMany({}).
+
+    {name:'mi note 11 pro max'},
+
+    { $set: {prize: 25000} }
+
+
+  )
+
+  console.log(data);
+
+}
+
+const deleteData = async()=>{
+
+  MongooseConnect();
+
+  const Product = mongoose.model('products',ProductSchema); // create model.
+
+  let data = await Product.deleteMany(  // as well as .deleteOne({});
+
+    {prize: 23000}
+  )
+  
+  console.log(data);
+  
+
+}
+
+
+const findData = async()=>{
+
+    MongooseConnect();
+  const Product = mongoose.model('products',ProductSchema); // create model.
+
+let data = await Product.find(  // also use ==>  find()  for saw all data
+  {name:'mi', price:5000} // here we find specific data
+);
+
+console.log(data);
+
+
+}
+
+// insetData();
+// updateData();
+// deleteData();
+// findData();
