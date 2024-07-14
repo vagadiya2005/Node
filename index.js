1af69021183f8ee204a43a116ad326e89e2b6e23
vagadiya2005 <princevagadiya2005@gmail.com>
2024-07-14 09:51:12 +0530

    craete all 4 API using mongoose

--- index.js
+++ index.js
@@ -7,12 +7,45 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/create", async(req, res) => {
-  res.send("done...");
-  
-  let data =  new Product(req.body);
-  let result = await data.save();
- 
+
+app.post('/',async(req,res)=>{  // POST API
+
+const data = await Product.insertMany(req.body);
+res.send(data);
+console.log(data);
+
+
+});
+
+app.get('/',async(req,res)=>{  // GET API
+
+  const products = await Product.find({name: 'M 34'}); // get data from DB or find specefic data.
+  res.send(products);
+  console.log(products);
+
+});
+
+app.put('/',async(req,res)=>{  // PUT API
+
+  const data = await Product.updateOne(  // as well as use .updateMany().
+
+    {name: 'M 34'},
+    {$set: {name: 'S 22'}}
+
+  )
+
+  res.send(data);
+  console.log(data);
+
+});
+
+app.delete('/',async(req,res)=>{ // DELETE API
+
+  const data = await Product.deleteOne(req.body);
+  res.send(data);
+  console.log(data);
+
+
 
 });
 
