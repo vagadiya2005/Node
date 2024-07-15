45c35ca6da65c998c83de829009d3920b02971e7
vagadiya2005 <princevagadiya2005@gmail.com>
2024-07-15 21:37:43 +0530

    Events in node.js

--- index.js
+++ index.js
@@ -1,64 +1,30 @@
-// const express = require("express");
-// const multer = require("multer");
-// const path = require('path');
+const express = require("express");
+const EventEmiter = require("events");
+const app = express();
 
-// const app = express();
+const event = new EventEmiter();
 
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, './uploads/');
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, Date.now() + path.extname(file.originalname));
-//     },
-//   }),
-// }).single('firstfile');
+let count = 0;
 
+event.on("apicalled", () => {  // when event occure then execute this code.
+    
+  count++;
+  console.log("api called", count); 
+});
 
+app.get("/root", (req, res) => {
+  res.send("root api");
+  event.emit("apicalled");  // creating event which handle by event.on()
+});
 
-// app.post("/upload", upload,(req, res) => {
-//   res.send("file uploaded");
-//   console.log('File uploaded:', req.file);
-// });
+app.get("/upload", (req, res) => {
+  res.send("upload api");
+  event.emit("apicalled");
+});
 
-// app.listen(5600);
+app.get("/abou", (req, res) => {
+  res.send("about api");
+  event.emit("apicalled");
+});
 
-// const express = require('express');
-// const multer = require('multer');
-
-// const app=express();
-
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, '/tmp/my-uploads')
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//     cb(null, file.fieldname + '-' + uniqueSuffix)
-//   }
-// })
-
-// const upload = multer({ storage: storage })
-
-
-// app.post('/upload',upload.single('file'),(req,res)=>{
-
-// res.send('file uploaded');
-// console.log('File uploaded:', req.file);
-
-// })
-
-
-// app.listen(5600);
-
-const os = require('os');
-
-console.log(os.arch());  //  give Architecturet of our system like   x32  or  x64.
-console.log(os.freemem()); // free memory in bytes.  
-console.log(os.hostname());  // hostname of system.
-console.log(os.platform());  // win32
-console.log(os.userInfo());
-
-
+app.listen(5600);
