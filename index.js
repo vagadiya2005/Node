83144e4148d3d3e41cfbae4c5e244cd46be0524f
Madhav Desai <md@Madhavs-MacBook-Air.local>
2024-07-30 18:05:44 +0530

    added cors.

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ require('./Database/config');
 const app = express();
 const cors = require('cors');
 const port = 5600;
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET","POST","DELETE","PUT"],
+}
+
+
+));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 const user = require('./Database/User');
@@ -11,30 +23,30 @@ const product = require('./Database/Product');
 
 app.use(express.json());
 
-app.post('/register',async(req,res)=>{
+app.post('/register', async (req, res) => {
 
-let data = new user(req.body);
-let result = await data.save();
-result = result.toObject();
-delete result.password;
-console.log(result);
+    let data = new user(req.body);
+    let result = await data.save();
+    result = result.toObject();
+    delete result.password;
+    console.log(result);
 
 })
 
-app.post('/login',async(req,res)=>{
+app.post('/login', async (req, res) => {
 
-let data = await user.findOne(req.body).select("-password");
+    let data = await user.findOne(req.body).select("-password");
 
-if(data){
-    res.send({message:"Login Successfull",data:data})
-}
-else{
-    res.send({message:"Login Failed",data:data})
-}
+    if (data) {
+        res.send({ message: "Login Successfull", data: data })
+    }
+    else {
+        res.send({ message: "Login Failed", data: data })
+    }
 
 });
 
-app.post('/add-product',async(req,res)=>{
+app.post('/add-product', async (req, res) => {
 
     let data = new product(req.body);
     let result = await data.save();
@@ -44,18 +56,18 @@ app.post('/add-product',async(req,res)=>{
 
     // console.log(JSON.stringify(result));
 
-});              
+});
 
-app.get('/products/:category',async(req,res)=>{
+app.get('/products/:category', async (req, res) => {
 
     let tag = req.params.category;
 
-    let data = await product.find({Tag: tag});
+    let data = await product.find({ Tag: tag });
 
-     if(data.length>0){
+    if (data.length > 0) {
         res.send(data)
-    }else{
-        res.send({result:"No data Found"});
+    } else {
+        res.send({ result: "No data Found" });
     }
 
 
